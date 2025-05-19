8af6fd15a76260a3e2953f49ffd94e282bf9084e
Quin Coolen <quin.coolen@gmail.com>
2025-05-19 12:09:29 +0200

    Add validation to auctiondto

--- GoonAuctionBLL/Dto/AuctionDto.cs
+++ GoonAuctionBLL/Dto/AuctionDto.cs
@@ -1,13 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoonAuctionBLL.Dto {
   public class AuctionDto {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "Description is required.")]
+    [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
     public string Description { get; set; }
+    [Required(ErrorMessage = "Starting price is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Starting price must be greater than 0.")]
+    [DataType(DataType.Currency)]
     public int StartingPrice { get; set; }
     public int CurrentPrice { get; set; }
     public string ImageUrl { get; set; }
+    [Required(ErrorMessage = "End date is required.")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [Range(typeof(DateTime), "1/1/2025", "12/31/2026", ErrorMessage = "End date must be between 1/1/2025 and 12/31/2026.")]
+    
     public DateTime EndDate { get; set; }
   }
 }
