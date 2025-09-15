a7be72c7bbe5a2d7a45a4763d67af3881ef742e4
Quin Coolen <quin.coolen@gmail.com>
2025-09-15 22:55:00 +0200

    Run migrations automatically

--- GoonAuctionAPI/Program.cs
+++ GoonAuctionAPI/Program.cs
@@ -105,6 +105,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
